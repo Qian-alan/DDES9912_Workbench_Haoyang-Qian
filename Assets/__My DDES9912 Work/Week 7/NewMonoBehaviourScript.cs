3d17a55e416493adef18a0f82db5a036fdfb6ef9
Qian-alan <qianhy123_hf@163.com>
2025-11-18 22:14:47 +1100

    review from week9

---
+++ Assets/__My DDES9912 Work/Week 7/NewMonoBehaviourScript.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GhostWalk : MonoBehaviour
+{
+    [Header("Navigation Settings")]
+    public Transform target;
+    public NavMeshAgent myagent;
+    public SinYBob characterBouncer;
+    public SinYBob [] hairBouncers;
+
+    [Header("Animation Settings")]
+    public float charaterBouncerSpeed= 730f;
+    void Start()
+    {
+        myagent = GetComponent<NavMeshAgent>();
+        characterBouncer=GetComponent<SinYBob>();
+        characterBouncers= GetComponentsInChildren<SinYBob>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
